35290183f19c3d3e4ef6b8639b898d7485953184
zoli0829 <zoli0829@gmail.com>
2025-11-15 10:03:25 +0000

    created the ui

--- CurrencyConvertor/ContentView.swift
+++ CurrencyConvertor/ContentView.swift
@@ -7,12 +7,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    var body: some View {
-        Text("Hello World!")
-    }
+	
+	@State private var amount = ""
+	@State private var conversion = ""
+	
+	var body: some View {
+		VStack(alignment: .leading) {
+			Text("Amount")
+				.font(.system(size: 15))
+			
+			TextField("", text: $amount)
+				.font(.system(size: 18, weight: .semibold))
+				.padding()
+				.overlay {
+					RoundedRectangle(cornerRadius: 5)
+						.fill(.clear)
+						.stroke(Color.gray, lineWidth: 1)
+				}
+			
+			HStack {
+				Spacer()
+				
+				Image(systemName: "arrow.up.arrow.down")
+					.font(.system(size: 20, weight: .bold))
+					.padding(.vertical)
+				
+				Spacer()
+			}
+			
+			Text("Converted To")
+				.font(.system(size: 15))
+			
+			TextField("", text: $conversion)
+				.font(.system(size: 18, weight: .semibold))
+				.padding()
+				.overlay {
+					RoundedRectangle(cornerRadius: 5)
+						.fill(.clear)
+						.stroke(Color.gray, lineWidth: 1)
+				}
+			HStack {
+				Spacer()
+				
+				Text("1.00000 USD = 0.85000 EUR")
+					.font(.system(size: 18, weight: .semibold))
+					.padding(.top, 25)
+				
+				Spacer()
+			}
+			
+		}
+		.padding(.horizontal)
+	}
 }
 
 #Preview {
-    ContentView()
+	ContentView()
 }
