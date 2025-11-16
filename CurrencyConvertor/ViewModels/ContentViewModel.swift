d431110a350447895d3d9a07f2e39bafc503746e
zoli0829 <zoli0829@gmail.com>
2025-11-16 15:25:25 +0000

    Added loading and error state to currency converter
    Introduces isLoading and errorMessage properties to ContentViewModel for improved user feedback during API requests. ContentView now displays a loading overlay and error messages, enhancing the UI/UX for network operations and error handling.

--- CurrencyConvertor/ViewModels/ContentViewModel.swift
+++ CurrencyConvertor/ViewModels/ContentViewModel.swift
@@ -14,6 +14,8 @@ class ContentViewModel: ObservableObject {
 	@Published var baseCurrency: CurrencyChoice = .Usa
 	@Published var convertedCurrency: CurrencyChoice = .Usa
 	@Published var rates: Rates?
+	@Published var isLoading = false
+	@Published var errorMessage = ""
 	
 	var numberFormatter: NumberFormatter {
 		let numberFormatter = NumberFormatter()
@@ -24,15 +26,22 @@ class ContentViewModel: ObservableObject {
 	
 	func fetchRates() async {
 
-		guard let url = URL(string: "https://openexchangerates.org/api/latest.json?app_id=\(OpenExchangeRates.appId)") else { return }
+		guard let url = URL(string: "https://openexchangerates.org/api/latest.json?app_id=\(OpenExchangeRates.appId)") else {
+			errorMessage = "Could not fetch rates."
+			print("API url is not valid")
+			return
+		}
 		let urlRequest = URLRequest(url: url)
 		
+		isLoading = true
 		do {
 			let (data, _) = try await URLSession.shared.data(for: urlRequest)
 			rates = try JSONDecoder().decode(Rates.self, from: data)
 		} catch {
+			errorMessage = "Could not fetch rates."
 			print(error.localizedDescription)
 		}
+		isLoading = false
 	}
 	
 }
--- CurrencyConvertor/Views/ContentView.swift
+++ CurrencyConvertor/Views/ContentView.swift
@@ -12,115 +12,135 @@ struct ContentView: View {
 	@State private var amount = ""
 	
 	var body: some View {
-		VStack(alignment: .leading) {
-			Text("Amount")
-				.font(.system(size: 15))
-			
-			TextField("", value: $viewModel.baseAmount, formatter: viewModel.numberFormatter)
-				.font(.system(size: 18, weight: .semibold))
-				.padding()
-				.overlay {
-					RoundedRectangle(cornerRadius: 5)
-						.fill(.clear)
-						.stroke(Color.gray, lineWidth: 1)
+		ZStack {
+			VStack(alignment: .leading) {
+				HStack {
+					Spacer()
+					
+					Text(viewModel.errorMessage)
+						.foregroundStyle(.red)
+						.font(.system(size: 18, weight: .semibold))
+					
+					Spacer()
 				}
-				.overlay(alignment: .trailing) {
-					HStack {
-						viewModel.baseCurrency.image()
-							.resizable()
-							.scaledToFill()
-							.frame(width: 30, height: 30)
-							.clipShape(Circle())
-						
-						Menu {
-							ForEach(CurrencyChoice.allCases) { currencyChoice in
-								Button {
-									viewModel.baseCurrency = currencyChoice
-								} label: {
-									Text(currencyChoice.fetchMenuName())
-								}
-
-							}
-						} label: {
-							Text(viewModel.baseCurrency.rawValue)
-								.font(.system(size: 16, weight: .bold))
-								.foregroundStyle(.black)
-							
-							Image(systemName: "chevron.down")
-								.font(.system(size: 16, weight: .bold))
-								.foregroundStyle(.black)
-						}
-
-					}
-					.padding(.trailing)
-				}
-			
-			HStack {
-				Spacer()
+				Text("Amount")
+					.font(.system(size: 15))
 				
-				Image(systemName: "arrow.up.arrow.down")
-					.font(.system(size: 20, weight: .bold))
-					.padding(.vertical)
-				
-				Spacer()
-			}
-			
-			Text("Converted To")
-				.font(.system(size: 15))
-			
-			TextField("", value: $viewModel.convertedAmount, formatter: viewModel.numberFormatter)
-				.font(.system(size: 18, weight: .semibold))
-				.padding()
-				.overlay {
-					RoundedRectangle(cornerRadius: 5)
-						.fill(.clear)
-						.stroke(Color.gray, lineWidth: 1)
-				}
-				.overlay(alignment: .trailing) {
-					HStack {
-						viewModel.convertedCurrency.image()
-							.resizable()
-							.scaledToFill()
-							.frame(width: 30, height: 30)
-							.clipShape(Circle())
-						
-						Menu {
-							ForEach(CurrencyChoice.allCases) { currencyChoice in
-								Button {
-									viewModel.convertedCurrency = currencyChoice
-								} label: {
-									Text(currencyChoice.fetchMenuName())
-								}
-
-							}
-						} label: {
-							Text(viewModel.convertedCurrency.rawValue)
-								.font(.system(size: 16, weight: .bold))
-								.foregroundStyle(.black)
-							
-							Image(systemName: "chevron.down")
-								.font(.system(size: 16, weight: .bold))
-								.foregroundStyle(.black)
-						}
-
-					}
-					.padding(.trailing)
-				}
-			
-			HStack {
-				Spacer()
-				
-				Text("1.00000 USD = 0.85000 EUR")
+				TextField("", value: $viewModel.baseAmount, formatter: viewModel.numberFormatter)
 					.font(.system(size: 18, weight: .semibold))
-					.padding(.top, 25)
+					.padding()
+					.overlay {
+						RoundedRectangle(cornerRadius: 5)
+							.fill(.clear)
+							.stroke(Color.gray, lineWidth: 1)
+					}
+					.overlay(alignment: .trailing) {
+						HStack {
+							viewModel.baseCurrency.image()
+								.resizable()
+								.scaledToFill()
+								.frame(width: 30, height: 30)
+								.clipShape(Circle())
+							
+							Menu {
+								ForEach(CurrencyChoice.allCases) { currencyChoice in
+									Button {
+										viewModel.baseCurrency = currencyChoice
+									} label: {
+										Text(currencyChoice.fetchMenuName())
+									}
+									
+								}
+							} label: {
+								Text(viewModel.baseCurrency.rawValue)
+									.font(.system(size: 16, weight: .bold))
+									.foregroundStyle(.black)
+								
+								Image(systemName: "chevron.down")
+									.font(.system(size: 16, weight: .bold))
+									.foregroundStyle(.black)
+							}
+							
+						}
+						.padding(.trailing)
+					}
 				
-				Spacer()
+				HStack {
+					Spacer()
+					
+					Image(systemName: "arrow.up.arrow.down")
+						.font(.system(size: 20, weight: .bold))
+						.padding(.vertical)
+					
+					Spacer()
+				}
+				
+				Text("Converted To")
+					.font(.system(size: 15))
+				
+				TextField("", value: $viewModel.convertedAmount, formatter: viewModel.numberFormatter)
+					.font(.system(size: 18, weight: .semibold))
+					.padding()
+					.overlay {
+						RoundedRectangle(cornerRadius: 5)
+							.fill(.clear)
+							.stroke(Color.gray, lineWidth: 1)
+					}
+					.overlay(alignment: .trailing) {
+						HStack {
+							viewModel.convertedCurrency.image()
+								.resizable()
+								.scaledToFill()
+								.frame(width: 30, height: 30)
+								.clipShape(Circle())
+							
+							Menu {
+								ForEach(CurrencyChoice.allCases) { currencyChoice in
+									Button {
+										viewModel.convertedCurrency = currencyChoice
+									} label: {
+										Text(currencyChoice.fetchMenuName())
+									}
+									
+								}
+							} label: {
+								Text(viewModel.convertedCurrency.rawValue)
+									.font(.system(size: 16, weight: .bold))
+									.foregroundStyle(.black)
+								
+								Image(systemName: "chevron.down")
+									.font(.system(size: 16, weight: .bold))
+									.foregroundStyle(.black)
+							}
+							
+						}
+						.padding(.trailing)
+					}
+				
+				HStack {
+					Spacer()
+					
+					Text("1.00000 USD = 0.85000 EUR")
+						.font(.system(size: 18, weight: .semibold))
+						.padding(.top, 25)
+					
+					Spacer()
+				}
+				
 			}
-			
-		}
-		.padding(.horizontal)
-		.task {
-			await viewModel.fetchRates()
+			.padding(.horizontal)
+			.task {
+				await viewModel.fetchRates()
+			}
+			if viewModel.isLoading {
+				ZStack {
+					Color.black.opacity(0.3)
+						.ignoresSafeArea()
+					
+					ProgressView()
+						.tint(.white)
+				}
+			}
 		}
 	}
 }
